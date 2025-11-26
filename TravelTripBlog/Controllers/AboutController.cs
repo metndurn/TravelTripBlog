019c06a3831382ea6cb3c79e37e8587e1f186e9b
metndurn <mtndrn123@gmail.com>
2025-11-27 01:47:40 +0300

    #4 Asp.Net Mvc5 ile Tatil & Seyahat Sitesi Content Alanı

---
+++ TravelTripBlog/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TravelTripBlog.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: Hakkımızda
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
