cd65bb90b86a8192d8baeb0d7cdb9b57942954b7
metndurn <mtndrn123@gmail.com>
2025-12-10 16:51:24 +0300

    #13 Asp.Net Mvc5 ile Tatil Seyahat Sitesi Bloglar Sayfası

---
+++ TravelTripBlog/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripBlog.Models.Entities;
+
+namespace TravelTripBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+		// GET: Blog
+		Context blog = new Context();
+		public ActionResult Index()
+        {
+            var value = blog.Bloglars.ToList();
+			return View(value);
+        }
+    }
+}
