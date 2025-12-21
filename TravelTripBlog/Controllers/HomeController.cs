e5be460a7916ca20fe580dcf53b3aee504026626
metndurn <mtndrn123@gmail.com>
2025-12-22 00:03:38 +0300

    #21 Asp.Net Mvc5 ile Tatil Seyahat Sitesi Ana Sayfa Slider Resimleri Çekme

--- TravelTripBlog/Controllers/HomeController.cs
+++ TravelTripBlog/Controllers/HomeController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripBlog.Models.Entities;
 
 namespace TravelTripBlog.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+		// GET: Home
+		Context blog = new Context();
+		public ActionResult Index()
         {
-            return View();
+            var bloglar = blog.Bloglars.ToList();
+			return View(bloglar);
         }
     }
 }
